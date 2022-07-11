13a003373389707a08682c2a4316e19906c92873
praveenr0701 <108972275+praveenr0701@users.noreply.github.com>
2022-07-12 00:18:14 +0530

    Create Day_1_problem_3.c

---
+++ Day_1/Day_1_problem_3.c
@@ -0,0 +1,13 @@
+//Consider a character input and find its ASCII value. 
+
+
+#include <stdio.h>
+int main()
+{
+    char c = 'p';
+    printf("The ASCII value of %c is %d", c, c);
+    return 0;
+}
+  
+
+
